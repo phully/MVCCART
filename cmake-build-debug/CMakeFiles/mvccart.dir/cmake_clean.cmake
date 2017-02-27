c861950124d11c840e4c510f93100f7ee536e5bf
umairsheikh <umairsly@gmail.com>
2017-02-27 12:34:29 +0100

    Created Libart plain

---
+++ cmake-build-debug/CMakeFiles/mvccart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mvccart.dir/main.cpp.o"
+  "CMakeFiles/mvccart.dir/art.c.o"
+  "mvccart.pdb"
+  "mvccart"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mvccart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
